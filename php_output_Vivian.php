26c505b4d21a19ca9711b36e2db8c2c84208e236
Nwaokorovivian29 <133647221+Nwaokorovivian29@users.noreply.github.com>
2023-05-26 21:29:32 +0100

    Add files via upload

---
+++ php_output_Vivian.php
@@ -0,0 +1,11 @@
+<?php
+$name = "<h1>NWAOKORO VIVIAN OLUCHUKWU</h1>";
+$regno = "2021/HND/33765/CS<br>";
+$soo = "ANAMBRA STATE<br>";
+$project = "<strong>Design and Implementation of an
+Online Inmates Management System</strong>";
+echo $name;
+echo $regno;
+echo $soo;
+echo $project;
+?>
